62dfb5419bf8117939f2f29bcf4be019783b28ae
mangochui030624 <140477620+ShinMango99@users.noreply.github.com>
2023-10-17 03:51:52 +0900

    Bug Fixed
    Build Error

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress;
 
 public class GameManager : MonoBehaviour
 {
