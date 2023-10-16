cd2e50f9da5663452642ef165f2cdbe945b5bc57
Chaeyeon Lee <101448461+ccy-1219@users.noreply.github.com>
2023-10-16 18:30:28 +0900

    script update
    /

--- Figment/Assets/CCL/Dahye/Scripts/Player.cs
+++ Figment/Assets/CCL/Dahye/Scripts/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class Player : MonoBehaviour
 {
@@ -19,10 +20,16 @@ public class Player : MonoBehaviour
 
     private float lastDamageTime;
 
+    //깜빡
+    private bool isBlinking = false;
+    private SpriteRenderer playerRenderer;
+
     private void Start()
     {
         Rigidbody rb = GetComponent<Rigidbody>();
         rb.isKinematic = true;
+        //깜빡
+        playerRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -63,6 +70,15 @@ public class Player : MonoBehaviour
         HP -= damage;
         Debug.Log("Player HP : " + HP);
         //Camera.Instance.Shake(); // 카메라 쉐이크 기능 추가
+
+        //깜빡
+        if (!isBlinking)
+        {
+            StartCoroutine(BlinkPlayer());
+        }
+
+
+
         if (HP <= 0)
         {
             GameManager.Instance.GameOver();
@@ -81,5 +97,28 @@ public class Player : MonoBehaviour
         }
 
         // TODO: add sound
+    }
+
+    //깜빡
+    IEnumerator BlinkPlayer()
+    {
+        // 깜빡거리는 상태를 시작합니다.
+        isBlinking = true;
+
+        // 깜빡거리는 동작이 끝나야 할 시간을 설정합니다. 현재 시간에서 4초를 더한 값입니다.
+        float endTime = Time.time + 4f;
+
+        // 현재 시간이 깜빡거리는 동작이 끝나야 할 시간보다 작은 동안 반복합니다.
+        while (Time.time < endTime)
+        {
+            // 플레이어의 SpriteRenderer 활성 상태를 반전시켜 깜빡거리는 효과를 줍니다.
+            playerRenderer.enabled = !playerRenderer.enabled;
+            // 0.1초 동안 대기한 후 다음 반복을 진행합니다.
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        playerRenderer.enabled = true;
+        //종료
+        isBlinking = false;
     }
 }
