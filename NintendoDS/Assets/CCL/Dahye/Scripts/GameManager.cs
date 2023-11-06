bd41e3c258ecbd75856426ce7998dde39276a166
DahyeChung <lewachung@gmail.com>
2023-11-06 16:14:14 +0900

    GameManager : [fix] instance error [Todo] spawned obj event bug

--- NintendoDS/Assets/CCL/Dahye/Scripts/GameManager.cs
+++ NintendoDS/Assets/CCL/Dahye/Scripts/GameManager.cs
@@ -3,23 +3,22 @@ using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
+/*This script is for :
+ * Input system provider
+ * Event Handler 제공자 setting
+ */
 {
     //-------------외부참조-----------------------
+
     private static GameManager _instance; // 내부
     public static GameManager Instance // 외부
     {
         get
         {
             // 인스턴스가 없는 경우에 접근하려 하면 인스턴스를 할당해준다.
-            if (!_instance)
+            if (_instance is null)
             {
-                _instance = FindObjectOfType(typeof(GameManager)) as GameManager;
-
-                if (_instance == null)
-                {
-                    Debug.Log("no Singleton obj");
-                    _instance = new GameObject("GameManager").AddComponent<GameManager>();
-                }
+                _instance = FindObjectOfType<GameManager>();
             }
             return _instance;
         }
@@ -34,39 +33,37 @@ public class GameManager : MonoBehaviour
         // 인스턴스가 존재하는 경우 새로생기는 인스턴스를 삭제한다.
         else if (_instance != this)
         {
+            Debug.Log("게임매니저 인스턴스 중복! 삭제하겠습니당~");
             Destroy(gameObject);
         }
-        // 아래의 함수를 사용하여 씬이 전환되더라도 선언되었던 인스턴스가 파괴되지 않는다.
+        // 아래의 함수를 사용하여 씬이 전환되                                                                                                                                                                                                더라도 선언되었던 인스턴스가 파괴되지 않는다.
 
     }
+    //--------------------------------------
+    //Key Inputs
+    public string Akey = "Fire2";
+    public string Bkey = "Fire1";
+    public string Ykey = "Fire3";
+    public string Xkey = "Jump";
+    public string horizontal = "Horizontal";
+    public string vertical = "Vertical";
 
     //---------------------------------------------
-
-
-
-
-
-
-
-
-
-
-    //Unit Prefabs
-    //public GameObject[] unitPrefabsArray;
-    //var randomSpawn = Random.Range(0, unitPrefabsArray.length);
-    public GameObject unitPrefabs;
-
-    public Unit lastUnit;
-    public Transform unitGroups;
-
-    //Event Handler
+    //이벤트 핸들러
     public event EventHandler OnAbuttonPressed;
 
+    public GameObject unitPrefabs;
 
+    public Unit lastUnitPrefab;
+    public Transform unitGroups;
 
     void Start()
     {
         NextUnit();
+    }
+    void Update()
+    {
+        AButtonPressed();
     }
 
     Unit GetUnits()
@@ -78,20 +75,19 @@ public class GameManager : MonoBehaviour
         return instantUnit;
     }
 
-    void NextUnit()
+    void NextUnit() // 다음 유닛 생성 함수
     {
-        // 다음 유닛 생성 함수
         Unit newUnit = GetUnits(); // 새로운 유닛 생성을 위해 겟유닛 함수 호출
-        lastUnit = newUnit; // 생성된 유닛을 일시 보관
+        lastUnitPrefab = newUnit; // 생성된 유닛을 일시 보관
 
         StartCoroutine("WaitNext");
     }
 
     IEnumerator WaitNext()
     {
-        while (lastUnit != null)
+        while (lastUnitPrefab != null) // 드롭 완료 이후
         {
-            yield return null;
+            yield return null; // 무한반복 방지. 드롭 이후 나가라
 
         }
         yield return new WaitForSeconds(2.5f);
@@ -103,9 +99,12 @@ public class GameManager : MonoBehaviour
 
     public void AButtonPressed()
     {
-        OnAbuttonPressed?.Invoke(this, EventArgs.Empty);
-        if (OnAbuttonPressed == null)
-            Debug.Log("No subscribers for event");
+        if (Input.GetButtonDown(Akey))
+        {
+            OnAbuttonPressed?.Invoke(this, EventArgs.Empty);
+            if (OnAbuttonPressed == null) // 이벤트가 NULL 이면 
+                Debug.Log("No subscribers for event");
+        }
     }
 
 
--- NintendoDS/Assets/CCL/Dahye/Scripts/Unit.cs
+++ NintendoDS/Assets/CCL/Dahye/Scripts/Unit.cs
@@ -2,21 +2,17 @@ using System;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
-{
-    /* 이동과 드롭의 기능을 가진 유닛의 행동 스크립트
-    */
+/* 이동과 드롭의 기능을 가진 유닛의 행동 스크립트
+ * This script for : 
+ * left & right movement behaviour
+ * subscribe events <Drop>
+*/
 
-    //--------------------------------------
-    //Key Inputs
-    private string Akey = "Fire2";
-    private string Bkey = "Fire1";
-    private string Ykey = "Fire3";
-    private string Xkey = "Jump";
-    private string horizontal = "Horizontal";
-    private string vertical = "Vertical";
-    //--------------------------------------
+{
+
     //[SerializeField] private Vector2 thisObj;
-    [SerializeField] private float speed = 3.0f;
+    [SerializeField]
+    private float speed = 3.0f;
 
     private bool isMove;
     private bool inBox;
@@ -25,7 +21,6 @@ public class Unit : MonoBehaviour
     Rigidbody2D rigid;
     //Animator anim;
 
-    public event EventHandler OnAbuttonPressed;
 
     void Awake()
     {
@@ -34,75 +29,58 @@ public class Unit : MonoBehaviour
         rigid.simulated = false;
         isMove = true;
         inBox = false;
-        //anim = GetComponent<Animator>();
+        // OnEnable(); // Event Subscription ON! 
 
+        //anim = GetComponent<Animator>();
     }
 
     void Update()
     {
-
-        if (Input.GetButtonDown(Akey))
-        {
-            //OnAbuttonPressed?.Invoke(this, EventArgs.Empty);
-            GameManager.Instance.AButtonPressed();
-        }
-
+        //if (Input.GetButtonDown(GameManager.Instance.Akey))
+        //    GameManager.Instance.AButtonPressed();
     }
 
-    //지속적인 키 입력은 FixedUpdate에서 하는 것
-    void FixedUpdate()
+    void FixedUpdate() //지속적인 키 입력은 FixedUpdate
     {
         horizontalMove();
     }
-
     void horizontalMove()
     {
         if (isMove && inBox == false)
         {
-            var movement = Input.GetAxis(horizontal);
+            var movement = Input.GetAxis(GameManager.Instance.horizontal);
             transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
         }
-        else
-        { }
-
-
-
-
-
-
-        // [TODO] 드롭한 후에는 이동 컨트롤 불가 로직 추가
-        //  - 특정 range 안에서만 이동 가능 -> rigidbody simulation 온오프로 해결! 
-        //  - 박스 range 안에 감지 후 이동 불가능
-        //  - Screen tap printer 에서 스타일러 사용 시 인풋설정 변화하는 법?
-
-
     }
+
+
+    //-----------Event Subscribe-----------------------------------
+
     private void OnEnable()
     {
         GameManager.Instance.OnAbuttonPressed += HandleAbuttonPressed;
-        Debug.Log("Subscribed to OnAbuttonPressed event.");
+        Debug.Log("구독 온");
     }
-    private void OnDisable()
+    private void OnDisable() // A키 기능을 제거할 때 사용 
     {
         GameManager.Instance.OnAbuttonPressed -= HandleAbuttonPressed;
+        Debug.Log("구독 오프");
     }
-    private void HandleAbuttonPressed(object sender, EventArgs e)
+    public void HandleAbuttonPressed(object sender, EventArgs e) // A 키 누르면 뭐 할건데?
     {
-        if (GameManager.Instance.lastUnit == null) // 드롭을 하기위해선 오브젝트가 있어야하는데, 생성된 오브젝트가 null 이라면 return으로 나가고, 생성받고 와라.
+        // 드롭 하려면 오브젝트가 있어야하는데, null 이라면 return으로 나가고, 생성받고 다시와라.
+        if (GameManager.Instance.lastUnitPrefab == null)
             return;
-
-        Debug.Log("HandleAbuttonPressed method called.");
         Drop();
-        GameManager.Instance.lastUnit = null;
 
-
-        Debug.Log("이벤트 드롭 마무리");
+        Debug.Log("이벤트 드롭 후 Null 처리 끝 ");
     }
     public void Drop()
     {
         isMove = false;
         rigid.simulated = true;
-        Debug.Log("유닛 드롭 발동");
+        Debug.Log("드롭 발동");
+        GameManager.Instance.lastUnitPrefab = null;
         //오브젝트 드롭 sfx 추가
     }
 
@@ -111,4 +89,16 @@ public class Unit : MonoBehaviour
 
 
 }
+
+
+
+
+
+
+//  ***Study Notes***
+// [TODO] 드롭한 후에는 이동 컨트롤 불가 로직 추가
+//  - 특정 range 안에서만 이동 가능 -> rigidbody simulation 온오프로 해결! 
+//  - 박스 range 안에 감지 후 이동 불가능
+//  - Screen tap printer 에서 스타일러 사용 시 인풋설정 변화하는 법?
+
 
