3d6247fc83b694204ac7586d0af506dcab0e6d12
mangochui030624 <140477620+ShinMango99@users.noreply.github.com>
2023-11-24 13:10:36 +0900

    Base
    Nintendo DS Base

---
+++ Assets/DSPreview/Scripts/DSAxisLight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DSAxisLight : MonoBehaviour
+{
+    public string AxisName = "Vertical";
+    public bool CheckPositive = true;
+    public float DeadZoneValue = 0.1f;
+    Image TargetImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TargetImage = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float axisValue = Input.GetAxis(AxisName);
+        bool overThreshold = false;
+        if(CheckPositive)
+        {
+            overThreshold = axisValue > DeadZoneValue;
+        }
+        else
+        {
+            overThreshold = axisValue < -DeadZoneValue;
+        }
+
+        TargetImage.enabled = overThreshold;
+    }
+}
---
+++ Assets/DSPreview/Scripts/IDSTapListener.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDSTapListener
+{
+    void OnScreenTapDown(Vector2 tapPosition);
+    void OnScreenDrag(Vector2 tapPosition);
+    void OnScreenTapUp(Vector2 tapPosition);
+}
