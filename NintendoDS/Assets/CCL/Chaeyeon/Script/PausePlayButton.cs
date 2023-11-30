8e15ba353201c57c1c29549f2beca59d4cca2de0
Chaeyeon Lee <101448461+ccy-1219@users.noreply.github.com>
2023-11-30 23:53:54 +0900

    play pause funtion complete!

--- NintendoDS/Assets/CCL/Chaeyeon/Script/PausePlayButton.cs
+++ NintendoDS/Assets/CCL/Chaeyeon/Script/PausePlayButton.cs
@@ -1,21 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 
-public class PausePlayButton : MonoBehaviour
+public class PausePlayButton : MonoBehaviour, IDSTapListener
 {
-    public Button button;     
-    public Sprite pauseSprite; 
-    public Sprite playSprite; 
-    private bool isPaused = false; 
+    public Button button;
+    public Sprite pauseSprite;
+    public Sprite playSprite;
 
-  
+    private bool isPaused = false;
+
+    public void OnScreenTapDown(Vector2 tapPosition)
+    {
+        if (DSTapRouter.RectangleContainsDSPoint(GetComponent<RectTransform>(), tapPosition))
+        {
+            TogglePausePlay();
+        }
+    }
+
     public void TogglePausePlay()
     {
-        isPaused = !isPaused; 
-        Time.timeScale = isPaused ? 0 : 1; 
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0 : 1;
+        Debug.Log(isPaused);
 
         // update button image
         button.image.sprite = isPaused ? playSprite : pauseSprite;
     }
 
+    public void OnScreenDrag(Vector2 tapPosition)
+    {
+    }
+
+    public void OnScreenTapUp(Vector2 tapPosition)
+    {
+    }
 }
