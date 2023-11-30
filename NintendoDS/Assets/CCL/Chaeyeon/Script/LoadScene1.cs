ac1a297c566fa632c84734f9a97d2aed22aee8f3
Chaeyeon Lee <101448461+ccy-1219@users.noreply.github.com>
2023-12-01 00:13:09 +0900

    Add Home button

---
+++ NintendoDS/Assets/CCL/Chaeyeon/Script/LoadScene1.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene1 : MonoBehaviour, IDSTapListener
+{
+    public string SceneName;
+
+
+    public void loadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void OnScreenTapDown(Vector2 tapPosition)
+    {
+        if (DSTapRouter.RectangleContainsDSPoint(GetComponent<RectTransform>(), tapPosition))
+        {
+            loadScene("Home");
+
+        }
+    }
+
+    public void OnScreenDrag(Vector2 tapPosition)
+    {
+      //  throw new System.NotImplementedException();
+    }
+
+    public void OnScreenTapUp(Vector2 tapPosition)
+    {
+//throw new System.NotImplementedException();
+    }
+}
