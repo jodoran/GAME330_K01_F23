9b7471b7e0419ac7ad49177d84faa12077db88b5
Chaeyeon Lee <101448461+ccy-1219@users.noreply.github.com>
2023-11-30 23:44:46 +0900

    editscript

--- NintendoDS/Assets/CCL/Chaeyeon/Script/PausePlayButton.cs
+++ NintendoDS/Assets/CCL/Chaeyeon/Script/PausePlayButton.cs
@@ -3,24 +3,19 @@ using UnityEngine.UI;
 
 public class PausePlayButton : MonoBehaviour
 {
-    public GameObject buttonObject; // 버튼 오브젝트
-    public Texture pauseTexture;    // 일시 정지 이미지
-    public Texture playTexture;     // 재생 이미지
-    private bool isPaused = false;  // 게임의 일시 정지 상태
+    public Button button;     
+    public Sprite pauseSprite; 
+    public Sprite playSprite; 
+    private bool isPaused = false; 
 
-    // 버튼 클릭 시 호출될 함수
+  
     public void TogglePausePlay()
     {
-        isPaused = !isPaused; // 일시 정지 상태 토글
-        Time.timeScale = isPaused ? 0 : 1; // 게임 일시 정지 또는 재개
-        UpdateButtonTexture(); // 버튼 텍스쳐 업데이트
-    }
+        isPaused = !isPaused; 
+        Time.timeScale = isPaused ? 0 : 1; 
 
-    // 버튼 텍스쳐를 업데이트하는 함수
-    private void UpdateButtonTexture()
-    {
-        var buttonTexture = isPaused ? playTexture : pauseTexture;
-        buttonObject.GetComponent<Renderer>().material.mainTexture = buttonTexture;
+        // update button image
+        button.image.sprite = isPaused ? playSprite : pauseSprite;
     }
 
 }
