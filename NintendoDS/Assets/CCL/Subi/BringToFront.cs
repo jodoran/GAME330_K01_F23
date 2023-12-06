98d9bf6adf8877a6e878597ea400f43a8352a543
Subi Lee <slee316@gmu.edu>
2023-12-06 15:36:22 +0900

    .

--- NintendoDS/Assets/CCL/Subi/BringToFront.cs
+++ NintendoDS/Assets/CCL/Subi/BringToFront.cs
@@ -2,21 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BringToFront : MonoBehaviour
 {
+
     void Start()
     {
-        // CanvasRenderer 컴포넌트 가져오기
-        CanvasRenderer canvasRenderer = GetComponent<CanvasRenderer>();
+        // Canvas 컴포넌트 가져오기
+        Canvas canvas = GetComponent<Canvas>();
 
-        // 가장 맨 위로 배치 (sortingOrder를 다른 모든 UI 요소보다 크게 설정)
-        canvasRenderer.sortingOrder = int.MaxValue;
+        // Overlay 모드로 변경
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+
+        // Order in Layer를 1로 설정 (다른 UI 요소보다 큰 숫자로 설정)
+        canvas.sortingOrder = 1;
     }
 }
 
 // Update is called once per frame
-void Update()
-    {
-        
-    }
-}
+
+
+
