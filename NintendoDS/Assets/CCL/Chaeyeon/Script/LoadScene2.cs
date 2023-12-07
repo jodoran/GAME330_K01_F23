464859e0a2739d18f73fdf0b8f3866c34df38e73
Chaeyeon Lee <101448461+ccy-1219@users.noreply.github.com>
2023-12-07 17:37:42 +0900

    Cursor and Icon

---
+++ NintendoDS/Assets/CCL/Chaeyeon/Script/LoadScene2.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene2 : MonoBehaviour, IDSTapListener
+{
+    public string SceneName;
+
+
+    public void loadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void OnScreenTapDown(Vector2 tapPosition)
+    {
+        if (DSTapRouter.RectangleContainsDSPoint(GetComponent<RectTransform>(), tapPosition))
+        {
+            loadScene("howtoplayscene");
+
+        }
+    }
+
+    public void OnScreenDrag(Vector2 tapPosition)
+    {
+      //  throw new System.NotImplementedException();
+    }
+
+    public void OnScreenTapUp(Vector2 tapPosition)
+    {
+    //throw new System.NotImplementedException();
+    }
+}
--- NintendoDS/Assets/CCL/Chaeyeon/Script/QuitGame.cs
+++ NintendoDS/Assets/CCL/Chaeyeon/Script/QuitGame.cs
@@ -5,24 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class QuitGame : MonoBehaviour, IDSTapListener
 {
-    public void OnScreenTapDown(Vector2 tapPosition)
-    {
-        // 화면이 탭되었을 때 게임 종료
-        ExitGame();
-    }
-
     public void ExitGame()
     {
-        
-    #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-    #else
-        // 실제 애플리케이션에서는 애플리케이션 종료
         Application.Quit();
-    #endif
+
+        Debug.Log("Game is exiting");
     }
 
-    public void OnScreenDrag(Vector2 tapPosition)
+   
+
+    public void OnScreenTapDown(Vector2 tapPosition)
+    {
+        if (DSTapRouter.RectangleContainsDSPoint(GetComponent<RectTransform>(), tapPosition))
+        {
+            ExitGame(); // 게임 종료 메소드 호출
+        }
+    }
+
+        public void OnScreenDrag(Vector2 tapPosition)
     {
         //  throw new System.NotImplementedException();
     }
